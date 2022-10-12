5c1035e1d684ac6e8f3aec35d0b1a49a518d4ffd
Maria Pulido ruda <mariapulidoruda@MacBook-Air-de-Maria.local>
2022-10-12 16:19:54 +0200

    styles

--- app/components/Nav/nav.js
+++ app/components/Nav/nav.js
@@ -2,7 +2,6 @@ export const Nav = () => `
 
 <nav class="nav">
   <div class="container">
-    <h1 class="logo"><a href="/index.html">DARK</a></h1>
     <ul>
       <li><a href="#" class="current" id="homelink">Home</a></li>
       <li><a href="#" id="seasonslink">Seasons</a></li>
